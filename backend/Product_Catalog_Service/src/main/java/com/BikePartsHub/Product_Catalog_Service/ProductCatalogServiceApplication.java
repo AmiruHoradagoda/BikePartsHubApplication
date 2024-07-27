068ab408b45aad958733ccb364ea738fb7b8ce2a
AmiruHoradagoda <139224955+AmiruHoradagoda@users.noreply.github.com>
2024-07-27 17:54:32 +0530

    Initial commit for BikePartsHubApplication

---
+++ backend/Product_Catalog_Service/src/main/java/com/BikePartsHub/Product_Catalog_Service/ProductCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.BikePartsHub.Product_Catalog_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogServiceApplication.class, args);
+	}
+
+}
