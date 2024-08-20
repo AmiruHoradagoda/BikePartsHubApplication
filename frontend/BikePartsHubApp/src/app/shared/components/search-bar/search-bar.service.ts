4ccaf7a2fad9c13254a57122e65791d2fbc754b9
AmiruHoradagoda <139224955+AmiruHoradagoda@users.noreply.github.com>
2024-08-20 15:08:44 +0530

    chore: add missing files and commit unsent changes

--- frontend/BikePartsHubApp/src/app/shared/components/search-bar/search-bar.service.ts
+++ frontend/BikePartsHubApp/src/app/shared/components/search-bar/search-bar.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { ProductSearchDetail } from '../../../core/models/interface/ProductSearchDetail';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SearchBarService {
-  private apiUrl = 'http://localhost:8080/api/v1/product/getProductsByName'; 
+  private apiUrl = 'http://localhost:8080/api/v1/product/getProductsByName';
 
   constructor(private http: HttpClient) {}
 
@@ -14,12 +15,12 @@ export class SearchBarService {
     productName: string,
     activeState: boolean = true,
     size: number = 10
-  ): Observable<any> {
+  ): Observable<ProductSearchDetail[]> {
     let params = new HttpParams()
       .set('productName', productName)
       .set('activeState', activeState.toString())
       .set('size', size.toString());
 
-    return this.http.get(this.apiUrl, { params });
+    return this.http.get<ProductSearchDetail[]>(this.apiUrl, { params });
   }
 }
