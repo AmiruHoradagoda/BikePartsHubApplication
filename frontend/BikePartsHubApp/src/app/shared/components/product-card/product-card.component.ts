69238d9952d2fcdc13723177840091c77a10ba86
AmiruHoradagoda <139224955+AmiruHoradagoda@users.noreply.github.com>
2024-09-21 03:16:29 +0530

    feat: update product list UI design for improved layout and styling

--- frontend/BikePartsHubApp/src/app/shared/components/product-card/product-card.component.ts
+++ frontend/BikePartsHubApp/src/app/shared/components/product-card/product-card.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { ProductGet } from '../../../core/models/interface/Product';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
+  imports:[ CommonModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css',
 })
