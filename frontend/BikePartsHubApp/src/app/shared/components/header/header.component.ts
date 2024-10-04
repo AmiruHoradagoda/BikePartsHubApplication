0ae85c38718fac1dcf337466a63cb83af51fb1e1
AmiruHoradagoda <139224955+AmiruHoradagoda@users.noreply.github.com>
2024-10-04 22:18:33 +0530

    feat: add increment function for cart items

--- frontend/BikePartsHubApp/src/app/shared/components/header/header.component.ts
+++ frontend/BikePartsHubApp/src/app/shared/components/header/header.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SearchBarComponent } from "../search-bar/search-bar.component";
 import { NavComponent } from "../nav/nav.component";
+import { CartItem } from '../../../core/models/interface/CartItem';
+import { CartService } from '../../../pages/cart-page/cart.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [SearchBarComponent, NavComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  cartItemCount = 0; // Variable to hold the count of cart items
 
+  constructor(private cartService: CartService) {}
+  ngOnInit(): void {
+    // Subscribe to cartItems$ observable to get real-time updates
+    this.cartService.cartItems$.subscribe((items: CartItem[]) => {
+      this.cartItemCount = items.reduce(
+        (total, item) => total + item.quantity,
+        0
+      ); // Update the count
+    });
+  }
 }
