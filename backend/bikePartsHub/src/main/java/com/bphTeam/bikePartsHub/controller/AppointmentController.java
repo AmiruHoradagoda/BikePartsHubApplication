577ab60e7f0a1cc288ca21d80b1c3bd6a888a386
AmiruHoradagoda <139224955+AmiruHoradagoda@users.noreply.github.com>
2024-12-29 16:10:22 +0530

    feat: add security annotation to AppointmentController for access control

--- backend/bikePartsHub/src/main/java/com/bphTeam/bikePartsHub/controller/AppointmentController.java
+++ backend/bikePartsHub/src/main/java/com/bphTeam/bikePartsHub/controller/AppointmentController.java
@@ -6,6 +6,7 @@ import com.bphTeam.bikePartsHub.service.AppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -14,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/appointment")
 @CrossOrigin
+@PreAuthorize("permitAll()")
 public class AppointmentController {
 
     @Autowired
