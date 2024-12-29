143063565357cb8ff96c93969ba082f98afe8635
AmiruHoradagoda <139224955+AmiruHoradagoda@users.noreply.github.com>
2024-12-29 16:17:32 +0530

    refactor: streamline app module by removing unused components and integrating ProductPageModule and AdminPageModule

--- frontend/BikePartsHubApp/src/app/app.module.ts
+++ frontend/BikePartsHubApp/src/app/app.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
-import {
-  BrowserModule,
-  provideClientHydration,
-} from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,21 +7,25 @@ import { HeaderComponent } from './shared/components/header/header.component';
 import { NavComponent } from './shared/components/nav/nav.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { CommonModule } from '@angular/common';
-import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { AngularFireModule } from '@angular/fire/compat';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { AdminSideNavbarComponent } from "./pages/admin-page/admin-side-navbar/admin-side-navbar.component";
-import { ProductFormComponent } from "./pages/admin-page/product-form/product-form.component";
+import { ProductFormComponent } from './pages/admin-page/product-form/product-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
-import { ProductDetailsComponent } from './pages/product-page/product-details/product-details.component';
+import { ProductDetailsComponent } from './pages/product-page/components/product-details/product-details.component';
 import { UserPageComponent } from './pages/user-page/user-page.component';
-import { SearchBarComponent } from "./shared/components/search-bar/search-bar.component";
-
+import { SearchBarComponent } from './shared/components/search-bar/search-bar.component';
+import { AdminPageModule } from './pages/admin-page/admin-page.module';
+import { ProductPageModule } from './pages/product-page/product-page.module';
+import { ProductCardComponent } from './pages/product-page/components/product-card/product-card.component';
 
 @NgModule({
-  declarations: [AppComponent, AdminPageComponent, ProductDetailsComponent, UserPageComponent],
+  declarations: [
+    AppComponent,
+    ProductDetailsComponent,
+    UserPageComponent,
+  ],
   imports: [
     CommonModule,
     BrowserModule,
@@ -32,22 +33,23 @@ import { SearchBarComponent } from "./shared/components/search-bar/search-bar.co
     HeaderComponent,
     NavComponent,
     FooterComponent,
+    AdminPageModule,
+    ProductPageModule,
     AngularFireStorageModule,
     AngularFireModule.initializeApp({
-        apiKey: 'AIzaSyCjRYdIsP6hnMnski-spc1pWuJwjA_7mNI',
-        authDomain: 'bikepartshubapp.firebaseapp.com',
-        projectId: 'bikepartshubapp',
-        storageBucket: 'bikepartshubapp.appspot.com',
-        messagingSenderId: '778797142877',
-        appId: '1:778797142877:web:fd53dc57fcbe02e0f8ceb4',
-        measurementId: 'G-5FKWWRNX6C',
+      apiKey: 'AIzaSyCjRYdIsP6hnMnski-spc1pWuJwjA_7mNI',
+      authDomain: 'bikepartshubapp.firebaseapp.com',
+      projectId: 'bikepartshubapp',
+      storageBucket: 'bikepartshubapp.appspot.com',
+      messagingSenderId: '778797142877',
+      appId: '1:778797142877:web:fd53dc57fcbe02e0f8ceb4',
+      measurementId: 'G-5FKWWRNX6C',
     }),
-    AdminSideNavbarComponent,
     ProductFormComponent,
-    BrowserAnimationsModule, 
+    BrowserAnimationsModule,
     ToastrModule.forRoot(),
-    SearchBarComponent
-],
+    SearchBarComponent,
+  ],
   providers: [provideHttpClient(withFetch())],
   bootstrap: [AppComponent],
 })
