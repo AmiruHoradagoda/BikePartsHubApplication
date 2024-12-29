f03b2396fbcc45532b11819510583f5f69f0fe7f
AmiruHoradagoda <139224955+AmiruHoradagoda@users.noreply.github.com>
2024-12-29 16:10:03 +0530

    fix: update UserRepository import to UserRepo for consistency

--- backend/bikePartsHub/src/main/java/com/bphTeam/bikePartsHub/config/security/ApplicationConfig.java
+++ backend/bikePartsHub/src/main/java/com/bphTeam/bikePartsHub/config/security/ApplicationConfig.java
@@ -1,6 +1,6 @@
 package com.bphTeam.bikePartsHub.config.security;
 
-import com.bphTeam.bikePartsHub.user.UserRepository;
+import com.bphTeam.bikePartsHub.user.UserRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
-    private final UserRepository repository;
+    private final UserRepo repository;
 
     @Bean
     public UserDetailsService userDetailsService(){
