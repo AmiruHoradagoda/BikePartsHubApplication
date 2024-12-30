67f647a403a70e21fbfa8a03ad1300b5b71afcc7
AmiruHoradagoda <139224955+AmiruHoradagoda@users.noreply.github.com>
2024-12-31 00:55:32 +0530

    feat: update header component to display user's first name and improve HTML structure

--- frontend/BikePartsHubApp/src/app/shared/components/header/header.component.ts
+++ frontend/BikePartsHubApp/src/app/shared/components/header/header.component.ts
@@ -1,23 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { SearchBarComponent } from "../search-bar/search-bar.component";
+import { SearchBarComponent } from '../search-bar/search-bar.component';
 import { CartItem } from '../../../core/models/interface/CartItem';
 import { CartService } from '../../../pages/cart-page/cart.service';
+import { AuthService } from '../../../auth/auth.service';
 
 @Component({
   selector: 'app-header',
-  standalone: true,
-  imports: [SearchBarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent implements OnInit {
-selectProduct(_t11: any) {
-throw new Error('Method not implemented.');
-}
   cartItemCount = 0; // Variable to hold the count of cart items
-
-  constructor(private cartService: CartService) {}
+  userFirstName: string | null = null;
+  constructor(
+    private authService: AuthService,
+    private cartService: CartService
+  ) {}
   ngOnInit(): void {
+     this.authService.user$.subscribe((user) => {
+       this.userFirstName = user ? user.firstName : null;
+     });
     // Subscribe to cartItems$ observable to get real-time updates
     this.cartService.cartItems$.subscribe((items: CartItem[]) => {
       this.cartItemCount = items.reduce(
@@ -25,5 +27,6 @@ throw new Error('Method not implemented.');
         0
       ); // Update the count
     });
+    console.log(this.userFirstName);
   }
 }
