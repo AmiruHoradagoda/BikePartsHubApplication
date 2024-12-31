8fb181995f37b138f0b457b30819f01516dfda24
AmiruHoradagoda <139224955+AmiruHoradagoda@users.noreply.github.com>
2024-12-31 14:23:04 +0530

    feat: update header component to use routerLink and display current user name

--- frontend/BikePartsHubApp/src/app/shared/components/header/header.component.ts
+++ frontend/BikePartsHubApp/src/app/shared/components/header/header.component.ts
@@ -1,32 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { SearchBarComponent } from '../search-bar/search-bar.component';
 import { CartItem } from '../../../core/models/interface/CartItem';
 import { CartService } from '../../../pages/cart-page/cart.service';
 import { AuthService } from '../../../auth/auth.service';
+import { AuthenticationResponse } from '../../../auth/auth.models';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css',
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  cartItemCount = 0; // Variable to hold the count of cart items
-  userFirstName: string | null = null;
+  cartItemCount = 0;
+  currentUser: AuthenticationResponse | null = null;
+
   constructor(
     private authService: AuthService,
-    private cartService: CartService
+    private cartService: CartService,
   ) {}
+
   ngOnInit(): void {
-     this.authService.user$.subscribe((user) => {
-       this.userFirstName = user ? user.firstName : null;
-     });
-    // Subscribe to cartItems$ observable to get real-time updates
-    this.cartService.cartItems$.subscribe((items: CartItem[]) => {
-      this.cartItemCount = items.reduce(
-        (total, item) => total + item.quantity,
-        0
-      ); // Update the count
+    this.cartService.cartItems$.subscribe({
+      next: (items: CartItem[]) => {
+        this.cartItemCount = items.reduce(
+          (total, item) => total + item.quantity,
+          0
+        );
+      },
+      error: (error) => {
+        console.error('Error getting cart items:', error);
+      },
     });
-    console.log(this.userFirstName);
+
+    this.authService.currentUser$.subscribe({
+      next: (user) => {
+        this.currentUser = user;
+      },
+      error: (error) => {
+        console.error('Error getting current user:', error);
+      },
+    });
   }
 }
