2905b67160bd50c705953946d42221fbd6857340
AmiruHoradagoda <139224955+AmiruHoradagoda@users.noreply.github.com>
2024-12-31 18:33:54 +0530

    feat: enhance user page with user details display and logout functionality

--- frontend/BikePartsHubApp/src/app/pages/user-page/user-page.component.ts
+++ frontend/BikePartsHubApp/src/app/pages/user-page/user-page.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationResponse } from '../../auth/auth.models';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-user-page',
   templateUrl: './user-page.component.html',
-  styleUrl: './user-page.component.css'
+  styleUrls: ['./user-page.component.css'],
 })
-export class UserPageComponent {
+export class UserPageComponent implements OnInit {
+  currentUser: AuthenticationResponse | null = null;
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.authService.currentUser$.subscribe({
+      next: (user) => {
+        this.currentUser = user;
+      },
+      error: (error) => {
+        console.error('Error getting current user:', error);
+      },
+    });
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
