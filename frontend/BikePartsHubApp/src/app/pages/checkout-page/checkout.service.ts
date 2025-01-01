780c798efba3e3dfe74fce28b66369ede81b8e40
AmiruHoradagoda <139224955+AmiruHoradagoda@users.noreply.github.com>
2025-01-01 23:33:31 +0530

    feat: add CheckoutService for order management and API integration

---
+++ frontend/BikePartsHubApp/src/app/pages/checkout-page/checkout.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { AuthService } from '../../auth/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CheckoutService {
+  private apiUrl = `${environment.apiUrl}/api/v1/order`;
+
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  saveOrder(orderData: any): Promise<any> {
+    const headers = new HttpHeaders().set(
+      'Authorization',
+      `Bearer ${localStorage.getItem('access_token')}`
+    );
+
+    return this.http
+      .post(`${this.apiUrl}/save`, orderData, { headers })
+      .toPromise()
+      .then((response) => {
+        if (!response) {
+          throw new Error('No response from server');
+        }
+        return response;
+      })
+      .catch((error) => {
+        if (error.status === 401) {
+          return this.authService
+            .refreshToken()
+            .toPromise()
+            .then(() => this.saveOrder(orderData));
+        }
+        throw error;
+      });
+  }
+}
