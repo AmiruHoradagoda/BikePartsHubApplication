033e6a329e807e9af3c7bef7263b1ab45ca66058
AmiruHoradagoda <139224955+AmiruHoradagoda@users.noreply.github.com>
2025-01-06 22:53:22 +0530

    feat: add method to find orders by user in OrderRepo

--- backend/bikePartsHub/src/main/java/com/bphTeam/bikePartsHub/repository/OrderRepo.java
+++ backend/bikePartsHub/src/main/java/com/bphTeam/bikePartsHub/repository/OrderRepo.java
@@ -1,11 +1,15 @@
 package com.bphTeam.bikePartsHub.repository;
 
 import com.bphTeam.bikePartsHub.entity.Order;
+import com.bphTeam.bikePartsHub.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 @EnableJpaRepositories
 public interface OrderRepo extends JpaRepository<Order, Long> {
+    Set<Order> findOrderByUser(User user);
 }
