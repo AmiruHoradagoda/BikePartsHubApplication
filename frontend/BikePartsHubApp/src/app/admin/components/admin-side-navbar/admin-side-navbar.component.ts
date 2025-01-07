2bf802325e92db53c858de3ac24de2c0ad911a5c
AmiruHoradagoda <139224955+AmiruHoradagoda@users.noreply.github.com>
2025-01-08 01:36:58 +0530

    feat: add Logout option to the side navigation bar

--- frontend/BikePartsHubApp/src/app/admin/components/admin-side-navbar/admin-side-navbar.component.ts
+++ frontend/BikePartsHubApp/src/app/admin/components/admin-side-navbar/admin-side-navbar.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdminAuthService } from '../../auth-admin/auth-admin.service';
 
 @Component({
   selector: 'app-admin-side-navbar',
   templateUrl: './admin-side-navbar.component.html',
   styleUrl: './admin-side-navbar.component.css',
 })
-export class AdminSideNavbarComponent {}
+export class AdminSideNavbarComponent {
+  constructor(private authService: AdminAuthService, private router: Router) {}
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login-admin']);
+  }
+}
