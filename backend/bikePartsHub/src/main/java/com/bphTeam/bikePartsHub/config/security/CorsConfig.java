efec8fa0544234d57ec93f10bd87720bdb04e72b
A.H. Pathirathna <165395407+AsithaPathirathna@users.noreply.github.com>
2025-05-09 18:13:24 +0530

    Update CorsConfig.java

--- backend/bikePartsHub/src/main/java/com/bphTeam/bikePartsHub/config/security/CorsConfig.java
+++ backend/bikePartsHub/src/main/java/com/bphTeam/bikePartsHub/config/security/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        config.setAllowedOrigins(Collections.singletonList("https://bikepartshub.altero.dev"));
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.ORIGIN,
                 HttpHeaders.CONTENT_TYPE,
