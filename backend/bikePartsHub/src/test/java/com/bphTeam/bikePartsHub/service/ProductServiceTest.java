c593d42216438aed203b52ea7209005e61198b96
Supun1419 <supuna1419@gmail.com>
2025-05-10 06:32:34 +0530

    Add initial ProductServiceTest class

---
+++ backend/bikePartsHub/src/test/java/com/bphTeam/bikePartsHub/service/ProductServiceTest.java
@@ -0,0 +1,34 @@
+package com.bphTeam.bikePartsHub.service;
+
+import com.bphTeam.bikePartsHub.service.ProductService;
+import com.bphTeam.bikePartsHub.dto.request.productRequestDto.ProductSaveRequestDto;
+import com.bphTeam.bikePartsHub.dto.request.productRequestDto.ProductUpdateRequestDto;
+import com.bphTeam.bikePartsHub.dto.response.productResponseDto.ProductGetResponseDTO;
+import com.bphTeam.bikePartsHub.dto.pagenated.PaginatedResponseItemDTO;
+import com.bphTeam.bikePartsHub.dto.response.productResponseDto.ProductSearchResponseDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+// Java
+
+
+
+
+class ProductServiceTest {
+
+    private ProductService productService;
+
+    @BeforeEach
+    void setUp() {
+        productService = Mockito.mock(ProductService.class);
+    }
+
+
+}
