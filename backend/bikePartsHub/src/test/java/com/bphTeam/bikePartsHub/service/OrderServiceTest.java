71fbe2245334be42b584242a3fe96b0ad07ad7d5
HirushanS <www.sadishadissanayaka@gamail.com>
2025-05-10 06:30:16 +0530

    Initial OrderServiceTest

---
+++ backend/bikePartsHub/src/test/java/com/bphTeam/bikePartsHub/service/OrderServiceTest.java
@@ -0,0 +1,27 @@
+package com.bphTeam.bikePartsHub.service;
+
+import com.bphTeam.bikePartsHub.dto.pagenated.PaginatedOrderResponseWithDetailsDto;
+import com.bphTeam.bikePartsHub.dto.request.orderRequestDto.OrderSaveRequestDto;
+import com.bphTeam.bikePartsHub.dto.response.orderResponseDto.OrderResponseWithDetailsDto;
+import com.bphTeam.bikePartsHub.utils.OrderStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+
+class OrderServiceTest {
+
+    private OrderService orderService;
+
+    @BeforeEach
+    void setUp() {
+        orderService = Mockito.mock(OrderService.class);
+    }
+
+ 
+}
