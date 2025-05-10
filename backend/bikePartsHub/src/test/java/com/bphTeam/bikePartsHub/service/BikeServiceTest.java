70cc87016006f1bb85eb4b6d349a391b80e17021
HirushanS <www.sadishadissanayaka@gamail.com>
2025-05-10 06:56:38 +0530

    Initialize BikeServiceTest

---
+++ backend/bikePartsHub/src/test/java/com/bphTeam/bikePartsHub/service/BikeServiceTest.java
@@ -0,0 +1,26 @@
+package com.bphTeam.bikePartsHub.service;
+
+
+import com.bphTeam.bikePartsHub.dto.request.bikeRequestDto.BikeSaveRequestDto;
+import com.bphTeam.bikePartsHub.dto.request.bikeRequestDto.BikeUpdateRequestDto;
+import com.bphTeam.bikePartsHub.dto.response.BikeGetResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class BikeServiceTest {
+
+    private BikeService bikeService;
+
+    @BeforeEach
+    void setUp() {
+        bikeService = Mockito.mock(BikeService.class);
+    }
+
+}
